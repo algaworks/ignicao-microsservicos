628dd17d2862a152ab9aea17e698c2671dcca866
Alex Augusto <alexaugustoti@gmail.com>
2025-07-28 16:11:36 -0300

    aulas

--- clean-git-on-subfolders.sh
+++
@@ -1,4 +0,0 @@
-find ./* -type d -name ".git" -exec rm -rf "{}" \;
-find ./* -type f -name ".git" -exec rm -rf "{}" \;
-find ./* -type f -name ".gitmodules" -exec rm -rf "{}" \;
-find ./* -type d -name ".gradle" -exec rm -rf "{}" \;
